6c027a89e9358296832f8bd2f8d0bca00e602a23
Syed Abbad Hasan Bokhari <abbadhasan750@gmail.com>
2024-11-16 17:26:56 +0500

    v1

--- src/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const submenu = document.querySelector('.submenu');
-
-    menuToggle.addEventListener('click', (e) => {
-        e.preventDefault();
-        submenu.classList.toggle('active');
-    });
-});
